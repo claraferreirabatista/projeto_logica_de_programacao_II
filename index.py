2244c5712b0544a6ada9705c484f697a55e33996
Clara Ferreira Batista <ferreirabatistaclara@gmail.com>
2023-07-31 18:24:32 -0300

    update

--- index.py
+++ index.py
@@ -1,9 +1,13 @@
+from datetime import datetime
 import random  # Importando a biblioteca random para gerar o ID da venda
 
 # Definindo a classe Vendedor
+
+
 class Vendedor:
     def __init__(self, nome):
         self.nome = nome
+
 
 # Mock de vendedores (mantemos o mesmo do exemplo anterior)
 vendedores = (
@@ -14,11 +18,14 @@ vendedores = (
 )
 
 # Definindo a classe Produto
+
+
 class Produto:
     def __init__(self, id_produto, nome, valor):
         self.id_produto = id_produto
         self.nome = nome
         self.valor = valor
+
 
 # Mock de produtos (mantemos o mesmo do exemplo anterior)
 produtos = [
@@ -40,6 +47,8 @@ produtos = [
 ]
 
 # Definindo a classe Venda
+
+
 class Venda:
     def __init__(self, venda_ID, vendedor):
         self.venda_ID = venda_ID
@@ -57,28 +66,32 @@ class Venda:
             self.produtos[produto] = quantidade
         self.valor_total_da_venda += produto.valor * quantidade
 
+
 def validar_cpf(cpf):
     # Implementação básica de validação do CPF (apenas para fins ilustrativos)
-    cpf = cpf.replace('.', '').replace('-', '')  # Remover pontos e traços, se houver
+    # Remover pontos e traços, se houver
+    cpf = cpf.replace('.', '').replace('-', '')
     if not cpf.isdigit() or len(cpf) != 11:
         return False
     return True  # Retornar True se o CPF for válido
+
 
 def validar_data(data):
     # Implementação básica de validação da data (apenas para fins ilustrativos)
     try:
         dia, mes, ano = map(int, data.split('/'))
-        if 1 <= dia <= 31 and 1 <= mes <= 12 and 1900 <= ano <= 2100:
+        if 1 <= dia <= 31 and 1 <= mes <= 12 and 2010 <= ano <= 2023:
             return True
         else:
             return False
     except ValueError:
         return False
 
+
 # Mock de vendas (mantemos o mesmo do exemplo anterior)
 vendas = [
     {
-       "id_venda": 1001,
+        "id_venda": 1001,
         "Nome do Vendedor": vendedores[0].nome,
         "CPF do Comprador": "123.456.789-10",
         "Data da Venda": "01/07/2023",
@@ -134,7 +147,7 @@ vendas = [
         ],
         "total_da_venda": (produtos[3].valor + produtos[4].valor + produtos[5].valor)
     },
-     {
+    {
         "id_venda": 1004,
         "Nome do Vendedor": vendedores[0].nome,
         "CPF do Comprador": "555.666.777-88",
@@ -177,7 +190,7 @@ vendas = [
         ],
         "total_da_venda": (produtos[9].valor + produtos[10].valor)
     },
-     {
+    {
         "id_venda": 1006,
         "Nome do Vendedor": vendedores[3].nome,
         "CPF do Comprador": "777.888.999-00",
@@ -249,7 +262,7 @@ while True:
     if resposta not in {'s', 'sim'}:
         break
 
-    venda_ID = random.randint(1000, 9999)
+    venda_ID = random.randint(1008, 9999)
     vendedor_nome = input("Digite o nome do vendedor: ")
     vendedor_encontrado = None
     for vendedor in vendedores:
@@ -302,7 +315,8 @@ while True:
                 "nome_do_produto": produto.nome,
                 "valor_do_produto": produto.valor
             }
-            for produto, quantidade in nova_venda.produtos.items()  # Correção do método items() no lugar de values()
+            # Correção do método items() no lugar de values()
+            for produto, quantidade in nova_venda.produtos.items()
         ],
         "total_da_venda": nova_venda.valor_total_da_venda
     })
@@ -325,6 +339,7 @@ for venda in vendas:
     print(f"Total da Venda: R$ {venda['total_da_venda']:.2f}\n")
     print()
 
+
 def excluir_venda_por_id(id_venda):
     global vendas  # Permite que a função acesse a variável global 'vendas'
     for i, venda in enumerate(vendas):
@@ -332,6 +347,7 @@ def excluir_venda_por_id(id_venda):
             del vendas[i]
             return True
     return False
+
 
 while True:
     resposta = input("Deseja excluir uma venda? (s/n): ").lower()
@@ -346,19 +362,22 @@ while True:
 
 print("Obrigada por usar o programa!")
 
-from datetime import datetime
 
 # Função para converter a data digitada pelo usuário em formato de string para formato de data
+
 def converter_data(data_str):
     return datetime.strptime(data_str, '%d/%m/%Y').date()
 
 # Função para encontrar o vendedor com mais vendas e o produto mais vendido do mês e ano fornecidos
+
+
 def encontrar_melhor_vendedor_e_produto(vendas, ano, mes):
     # Converter a data do usuário para o mesmo formato das datas na lista de vendas
     data_procurada = converter_data(f'01/{mes}/{ano}')
 
     # Filtrar as vendas que correspondem ao mês e ano fornecidos pelo usuário
-    vendas_do_mes = [venda for venda in vendas if converter_data(venda["Data da Venda"]) == data_procurada]
+    vendas_do_mes = [venda for venda in vendas if converter_data(
+        venda["Data da Venda"]) == data_procurada]
 
     # Inicializar as variáveis para guardar as informações sobre o melhor vendedor e o produto mais vendido
     melhor_vendedor = None
@@ -387,10 +406,14 @@ def encontrar_melhor_vendedor_e_produto(vendas, ano, mes):
 
     return melhor_vendedor, total_vendas_melhor_vendedor, produto_mais_vendido["nome_do_produto"], quantidade_total_produto_mais_vendido
 
+
 ano_escolhido = int(input("Digite o ano desejado: "))
 mes_escolhido = int(input("Digite o mês desejado: "))
 
-melhor_vendedor, total_vendas_melhor_vendedor, produto_mais_vendido, quantidade_total_produto_mais_vendido = encontrar_melhor_vendedor_e_produto(vendas, ano_escolhido, mes_escolhido)
+melhor_vendedor, total_vendas_melhor_vendedor, produto_mais_vendido, quantidade_total_produto_mais_vendido = encontrar_melhor_vendedor_e_produto(
+    vendas, ano_escolhido, mes_escolhido)
 
-print(f"Melhor vendedor do mês {mes_escolhido}/{ano_escolhido}: {melhor_vendedor}, Total de vendas: R${total_vendas_melhor_vendedor:.2f}")
-print(f"Produto mais vendido do mês {mes_escolhido}/{ano_escolhido}: {produto_mais_vendido}, Quantidade total vendida: R${quantidade_total_produto_mais_vendido:.2f}")
+print(
+    f"Melhor vendedor do mês {mes_escolhido}/{ano_escolhido}: {melhor_vendedor}, Total de vendas: R${total_vendas_melhor_vendedor:.2f}")
+print(
+    f"Produto mais vendido do mês {mes_escolhido}/{ano_escolhido}: {produto_mais_vendido}, Quantidade total vendida: R${quantidade_total_produto_mais_vendido:.2f}")
