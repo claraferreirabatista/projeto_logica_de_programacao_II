b40aa64ade3c025243f5d3d2d1f124561b374536
Clara Ferreira Batista <ferreirabatistaclara@gmail.com>
2023-07-31 19:54:04 -0300

    add sell to menu

--- menu.py
+++ menu.py
@@ -1,22 +1,23 @@
 """
 """
+from vendas import inclui_venda
+
 while True:
-    resposta = input( "Escolha as opções, digite o respectivo número da sua opção: \n 1. Incluir venda? \n 2. Excluir venda? \n 3. Qual vendedor foi mais rentável? \n 4. Qual produto foi o mais vendido? \n 5. Sair do programa? \n "
+    resposta = input( "Escolha as opções, digite o respectivo número da sua opção: \n 1. Incluir venda? \n 2. Excluir venda? \n 3. Qual vendedor foi mais rentável? \n 4. Qual produto foi o mais vendido? \n 5. Mostrar todas as vendas? \n 6. Sair do programa? \n "
     )
     if resposta == '1':
         print("Opção escolhida 1")
-        break
+        inclui_venda()
     elif resposta == '2':
         print("Opção escolhida 2")
-        break
     elif resposta == '3':
         print("Opção escolhida 3")
-        break
     elif resposta == '4':
         print("Opção escolhida 4")
-        break
     elif resposta == '5':
         print("Opção escolhida 5")
+    elif resposta == '6':
+        print("Opção escolhida 6")
         break
     else:
         print("Código incorreto, digite novamente")
