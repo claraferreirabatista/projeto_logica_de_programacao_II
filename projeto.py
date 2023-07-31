99bce6aa2c08bbb803767b3092507f8888ed7b95
Clara Ferreira Batista <ferreirabatistaclara@gmail.com>
2023-07-30 21:43:25 -0300

    up

--- projeto.py
+++ projeto.py
@@ -1,9 +1,1 @@
-venda =  {
-        "ID": 1,
-        "Nome do produto": "Celular",
-        "Valor do produto": 1500.00,
-        "Nome do vendedor": "João",
-        "CPF do comprador": "123.456.789-00",
-        "Data da venda": "2023-07-30",
-        "Informações opcionais": "Frete incluso"
-    }
+
