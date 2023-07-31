6fac12cf3b67538f22cb17e5fa2d1faaa6ec7d5d
Clara Ferreira Batista <ferreirabatistaclara@gmail.com>
2023-07-31 19:33:53 -0300

    add_menu

---
+++ menu.py
@@ -0,0 +1,24 @@
+"""
+"""
+while True:
+    resposta = input( "Escolha as opções, digite o respectivo número da sua opção: \n 1. Incluir venda? \n 2. Excluir venda? \n 3. Qual vendedor foi mais rentável? \n 4. Qual produto foi o mais vendido? \n 5. Sair do programa? \n "
+    )
+    if resposta == '1':
+        print("Opção escolhida 1")
+        break
+    elif resposta == '2':
+        print("Opção escolhida 2")
+        break
+    elif resposta == '3':
+        print("Opção escolhida 3")
+        break
+    elif resposta == '4':
+        print("Opção escolhida 4")
+        break
+    elif resposta == '5':
+        print("Opção escolhida 5")
+        break
+    else:
+        print("Código incorreto, digite novamente")
+
+print("Obrigado por usar o programa")
