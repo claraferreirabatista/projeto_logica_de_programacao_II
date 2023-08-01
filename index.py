246c5fa1fe1b90d743d9bd294677ab5cadc6db82
Clara Ferreira Batista <ferreirabatistaclara@gmail.com>
2023-08-01 14:23:57 -0300

    update_new_functions

--- index.py
+++ index.py
@@ -1,19 +1,28 @@
 from add_purchase import inclui_venda
 from del_purchase import exclui_venda
+from best_seller import mostrar_melhores_vendedores
+from utils import obter_ano_mes
+from best_product import processar_vendas
 
 while True:
-    resposta = input( "Escolha as opções, digite o respectivo número da sua opção: \n 1. Incluir venda? \n 2. Excluir venda? \n 3. Qual vendedor foi mais rentável? \n 4. Qual produto foi o mais vendido? \n 5. Mostrar todas as vendas? \n 6. Sair do programa? \n "
-    )
+    resposta = input("""\n Escolha qual opção desejar:
+    \n 1. Gostaria de incluir uma venda?
+    \n 2. Ou, deseja de excluir uma venda?
+    \n 3. Saber quem foram os vendores que mais venderam?
+    \n 4. Ou, quer saber o nosso produto mais vendido?
+    \n 5. Mostrar todas as vendas?
+    \n 6. Sair do programa? \n """
+                     )
     if resposta == '1':
-        print("Opção escolhida 1")
         inclui_venda()
     elif resposta == '2':
-        print("Opção escolhida 2")
         exclui_venda()
     elif resposta == '3':
-        print("Opção escolhida 3")
+        mostrar_melhores_vendedores()
+        break
     elif resposta == '4':
-        print("Opção escolhida 4")
+        processar_vendas()
+        break
     elif resposta == '5':
         print("Opção escolhida 5")
     elif resposta == '6':
